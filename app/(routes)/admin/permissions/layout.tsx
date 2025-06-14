43c24ca5c8991b0b746bf09d0806f2f29394ae67
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2025-06-14 20:29:22 +0700

    fixed errors

--- app/(routes)/admin/permissions/layout.tsx
+++
@@ -1,5 +0,0 @@
-export default function UsersLayout({children}: {children: React.ReactNode}) {
-  Define breadcrumb items for users section
-  Breadcrumbs will be handled by AdminLayout now
-  return <div>{children}</div>;
-}
--- app/(routes)/admin/settings/permissions-management/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { PermissionsManagement } from '@/screens/admin/permissions/PermissionsManagement';
-
-export default function PermissionManagementPage() {
-  return <PermissionsManagement />;
-}
